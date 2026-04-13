93f89cd8b7f2db3e515795bb3904721b38eb5504
Satyamsingh202 <satyamsingh.uj@gmail.com>
2026-04-14 03:00:07 +0530

    make changes to run on my device

--- backend/venv/bin/activate.fish
+++ backend/venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/satyamkumarsingh/Desktop/Hackathons/Hackachinno 2/backend/venv"
+set -gx VIRTUAL_ENV "/Users/satyamkumarsingh/Desktop/Hackathons/Hackachinno 3/ParkEasy/backend/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
